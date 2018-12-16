9968cdda8d804f2fb1d8cb9f43e0e265b44dd424
Mohammed Tahmid <mohammedtahmid@live.co.uk>
2018-12-16 22:32:50 +0000

    Added the foundation for the login system.

---
+++ login.php
@@ -0,0 +1,136 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+
+// Include config file
+require_once "includes/config.php";
+ 
+// Define variables and initialize with empty values
+$email = $password = "";
+// $username = $password = $email = "";
+$email_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter email.";
+    } else{
+        $email = trim($_POST["email"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($email_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, firstName, lastName, email, gender, birthYear, cWeight, heightFeet, heightInch, bodyFat, gWeight, activityLevel, password FROM users WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = $email;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $firstName, $lastName, $email, $gender, $birthYear, $cWeight, $heightFeet, $heightInch, $bodyFat, $gWeight, $activityLevel, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            if(!isset($_SESSION)) { 
+                            	session_start();
+      						}                      
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["firstName"] = $firstName;   
+                            $_SESSION["email"] = $email;      
+                            $_SESSION["gender"] = $gender;  
+                            $_SESSION["birthYear"] = $birthYear; 
+                            $_SESSION["cWeight"] = $cWeight; 
+                            $_SESSION["heightFeet"] = $heightFeet; 
+                            $_SESSION["heightInch"] = $heightInch; 
+                            $_SESSION["bodyFat"] = $bodyFat; 
+                            $_SESSION["gWeight"] = $gWeight;
+                            $_SESSION["activityLevel"] = $activityLevel; 
+                            $_SESSION["lastName"] = $lastName;                    
+                            
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $email_err = "No account found with that email.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <style type="text/css">
+
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        	<div>
+        		<span class="help-block"><?php echo $email_err; ?></span>
+        		<span class="help-block"><?php echo $password_err; ?></span>
+        	</div>
+            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
